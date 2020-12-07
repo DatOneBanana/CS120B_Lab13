c5eeb21694e8c082ed5d935da8f859aaeadd989a
Ubuntu <datonearash@gmail.com>
2020-12-06 17:01:08 -0800

    work in progress

--- source/main.c
+++ source/main.c
@@ -10,14 +10,89 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+#include "bit.h"
+#include "timer.h"
+#include "scheduler.h"
 #endif
+
+
+void A2D_init()
+{
+	ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	// ADEN: Enables analog-to-digital conversion
+	// ADSC: Starts analog-to-digital conversion
+	// ADATE: Enables auto-triggering, allowing for constant
+	//	    analog to digital conversions.
+}
+void convert()
+{
+	ADCSRA |=(1<<ADSC);//start ADC conversion
+	while ( !(ADCSRA & (1<<ADIF)));//wait till ADC conversion
+
+}
+
+enum DisplayStates{init} state;
+unsigned short temp = 0;
+
+
+int tick(int state)
+{
+	switch(state)
+	{
+		case init:
+			state = init;
+			break;
+		default:
+			break;
+	}
+	switch(state)
+	{
+		case init:
+			convert();
+			temp = ADC;
+			PORTB = temp;
+			PORTD = temp >> 8;
+			break;
+		default:
+			break;
+	}
+	return state;
+}
+
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	DDRD = 0xFF; PORTD = 0x00;
+	
+	A2D_init();
 
-    /* Insert your solution below */
-    while (1) {
+	static task task1;
+	task *tasks[] = { &task1 };
+	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
 
-    }
+
+
+	task1.state = 0;
+	task1.period = 100;
+	task1.elapsedTime = task1.period;
+	task1.TickFct = &tick;
+
+	TimerSet(1);
+	TimerOn();
+
+    	unsigned short i;
+    	while (1) {
+		for(i = 0; i < numTasks; i++){
+			if(tasks[i]->elapsedTime == tasks[i]->period){
+				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+				tasks[i]->elapsedTime = 0;
+			}
+			tasks[i]->elapsedTime += 1;
+		}
+		while(!TimerFlag);
+		TimerFlag = 0;
+    	}
     return 1;
 }
